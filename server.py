0b4e22113f61bde66eb7407c0ef35b0c1970879a
srshchkv <125016193+srshchkv@users.noreply.github.com>
2024-04-02 20:42:19 +0300

    Multithread server.py

--- server.py
+++ server.py
@@ -1,20 +1,31 @@
-import socket
+import socket 
+import threading
+#Программа, написанная на семинаре выдавала ошибку при подключении нескольких клиентов,тк поток и функция для обработки подключения имели одинаковое название
+#nтеперь возможна работа с несколькими клиентами одновременно
+# Создаем сокет для сервера
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+server_socket.bind(('', 9091))  
+server_socket.listen(5)  
 
-msg = ''
+print("Эхо-сервер запущен, ожидаем подключений...")
+
+# Функция для обработки подключения клиента в отдельном потоке
+def client_threading(conn, addr):
+    print(f"Новое подключение: {addr}")
+    while True:
+         data = conn.recv(1024)
+         if not data:
+             break
+    # Отправляем данные обратно клиенту в верхнем регистре
+         conn.send(data.decode().upper().encode())
+    # Закрываем подключение при выходе из цикла
+    conn.close()
+    print(f"Соединение с клиентом {addr} закрыто.")
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
+     conn, addr = server_socket.accept()
+     # Создаем новый поток для обработки подключения
+     client_thread = threading.Thread(target=client_threading, args=(conn, addr))
+     client_thread.start()
